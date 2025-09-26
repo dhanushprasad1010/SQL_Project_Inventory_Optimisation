e0ad01d059e0992c9c06761064ffa3b7dd144dd2
dhanushprasad1010 <djprasad882@gmail.com>
2025-09-26 08:45:40 +0530

    commit

--- 4_ev_stock_by_category.sql
+++ 4_ev_stock_by_category.sql
@@ -1,4 +1,11 @@
-SELECT category, year, SUM(value) AS total_ev_stock
+SELECT 
+    mode AS category,      -- this will show Cars, 2/3 wheelers, Buses
+    year,
+    SUM(value) AS total_ev_stock
 FROM ev_stock
-GROUP BY category, year
-ORDER BY category, year;
+WHERE category = 'Historical'      -- filter only Historical rows
+GROUP BY 
+    mode, 
+    year
+ORDER BY 
+    year, total_ev_stock DESC;
